95dda177b31102b0a2f324348f446ea7084c1fb3
cvaquedano <chvb2002@hotmail.com>
2019-03-30 08:35:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const category = require('./category-query')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Expense Tracker Api' })
+  })
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
+  app.get('/category', category.getCategories)
+app.get('/category/:id', category.getCategoryById)
+app.post('/category', category.createCategory)
+app.put('/category/:id', category.updateCategory)
+app.delete('/category/:id', category.deleteCategory)
